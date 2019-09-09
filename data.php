473b4b790e7015aa9d1ce3cb8683f89c99ff542d
spencer-alabaugh <spenceralabaugh@gmail.com>
2019-09-09 13:23:26 -0400

    Vue started

---
+++ data.php
@@ -0,0 +1,5 @@
+[
+  {"patientGuid":"1", "firstName":"John", "lastName":"Doe", "dob": "1996-07-13", "sexAtBirth": "M"},
+  {"patientGuid":"2", "firstName":"Jane", "lastName":"Doe", "dob": "1996-08-13", "sexAtBirth": "F"},
+  {"patientGuid":"3", "firstName":"Steve", "lastName":"Smith", "dob": "2000-07-13", "sexAtBirth": "M"},
+]
